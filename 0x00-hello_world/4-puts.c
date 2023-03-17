dff4d5cbaff9c51890928e9682d15f341f503640
azemoure-1 <a.azemour-1@gmail.com>
2023-03-17 02:02:30 +0100

    preposission

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include < stdio.h>
+
+/**
+ * main - print the string in the put function
+ *
+ * description: using the main function 
+ * this program prints "Programming is lik building a multilingual puzzle
+ * return: 0"
+ */
+ * int main (void)
+ * {
+ * 	puts ("\"programming is like building a multilingual puzzle";
+	return (0)
+
+   }
+
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - print the string in the put function description:
+
+description og ptoduvt[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D*[C[Ddescription: using the main function
+*
+[A[A[A
+[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[:[B[B[D[D[D[D[D[D[D[D[D[D[D[C[C
+*this program prints with proper grammar, but the outcome id a piece of art,
+* return : 0
+[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D
+*/
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n"[C[C
