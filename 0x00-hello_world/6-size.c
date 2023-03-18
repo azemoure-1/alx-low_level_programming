a2af6e46afd5f0f42918064251af96b17b075d1b
azemoure-1 <a.azemour1@gmail.com>
2023-03-18 05:59:37 -0700

    modified size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 
 /**
-* You should produce the exact same output as in the example
-* Warnings are allowed
+*main - print the string in the put function
 *
-* You program should return 0
+* Description: using the main function 
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
 */
 int main(void)
 {
-	char c;
-	int i;
-	long li;
-	long long lli;
-	float f;
+        char C;
+        int i;
+        long li;
+        long long lli;
+        floatf f;
 
-	printf("size of a char: %ld byte(s)\n", sizeof(c));
-	printf("size of a int: %ld byte(s)\n", sizeof(i));
-	printf("size of a long: %ld byte(s)\n", sizeof(li));
-	printf("size of a long long: %ld byte(s)\n", sizeof(lli));
-	printf("size of a flot: %ld byte(s)\n", sizeof(f));
-	return (0);
+        printf("Size of a char: %ld byte(s)\n", sizeof(C));
+        printf("Size of an int: %ld byte(s)\n", sizeof(i));
+        printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+        printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+        printf("Size of a float: %ld byte(s)\n", sizeof(f));
+        return (0);
 }
