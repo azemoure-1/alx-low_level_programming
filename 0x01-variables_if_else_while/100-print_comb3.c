39505ce669ae17f73ea6431eb118b55ddc8a26c7
azemoure-1 <a.azemour1@gmail.com>
2023-03-18 16:17:36 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,26 +3,31 @@
 /**
  * main - Entry point
  *
+ * Description: prints all possible different combinations of two digits.
+ *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i <= 8; i++)
-    {
-        for (j = i + 1; j <= 9; j++)
-        {
-            putchar(i + '0');
-            putchar(j + '0');
-            if (i != 8 || j != 9)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-    putchar('\n');
+	for (i = 0; i <= 8; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+			putchar((i % 10) + '0');
+			putchar((j % 10) + '0');
 
-    return (0);
+			if (i == 8 && j == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
 }
