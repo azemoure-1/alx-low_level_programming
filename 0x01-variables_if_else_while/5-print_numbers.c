9146f69e6d25927dec69efe62f91d9c0ae936680
azemoure-1 <a.azemour1@gmail.com>
2023-03-18 15:14:15 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,18 +3,19 @@
 /**
  * main - Entry point
  *
+ * Description: prints all single digit numbers of base 10 starting from 0
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-        int i;
+	int i;
 
-        for (i = 0; i < 10; i++)
-        {
-                putchar(i + '0');
-        }
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
 
-        putchar('\n');
-
-        return (0);
+	return (0);
 }
