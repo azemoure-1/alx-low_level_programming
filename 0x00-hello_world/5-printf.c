c710f8c901eec8ba40d0f214d7a2692af7e61cc6
azemoure-1 <a.azemour1@gmail.com>
2023-03-18 07:13:21 -0700

    5 modified

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
-* main - print the string in the put function 
-*
-* Description: using the main function 
-* this program prints with proper grammar, but outcome is a piece of art,
-* Return: 0
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
