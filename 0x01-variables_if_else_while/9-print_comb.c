a03c4af54d3fc4e128ce747b0959a6b81a37a3eb
azemoure-1 <a.azemour1@gmail.com>
2023-03-18 16:04:41 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,20 +3,17 @@
 /**
  * main - Entry point
  *
- * Description: Prints all possible combinations of single-digit numbers
- * separated by ", " in ascending order
- *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-    int num;
+    int i;
 
-    for (num = 0; num < 10; num++)
+    for (i = 0; i < 10; i++)
     {
-        putchar(num + '0');
-        if (num != 9)
+        putchar(i + '0');
+
+        if (i != 9)
         {
             putchar(',');
             putchar(' ');
@@ -24,5 +21,6 @@ int main(void)
     }
 
     putchar('\n');
+
     return (0);
 }
