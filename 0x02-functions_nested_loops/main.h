92a21332535baea0781cb2e9c11bed8344b45921
azemoure-1 <a.azemour1@gmail.com>
2023-03-21 03:12:43 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,13 +8,13 @@
 
 int main(void)
 {
-    char str[] = "_putchar\n";
-    int i;
+	char str[] = "_putchar\n";
+	int i;
 
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        _putchar(str[i]);
-    }
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
 
-    return (0);
+	return (0);
 }
