1ac74b3af09f37cb8c4a9f922af949ef4b14c8de
azemoure-1 <a.azemour1@gmail.com>
2023-03-21 16:43:11 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,17 @@
-frf
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+    int ch;
+
+    for (ch = 'a'; ch <= 'z'; ch++)
+    {
+        _putchar(ch);
+    }
+    _putchar('\n');
+}
