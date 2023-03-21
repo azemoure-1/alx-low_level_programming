a2944ead30e895c929744cd7702484c1e253d3b4
azemoure-1 <a.azemour1@gmail.com>
2023-03-21 03:56:03 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Entry point
- * Description: Write a program that prints _putchar, followed by a new line.
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -16,5 +16,6 @@ int main(void)
     _putchar('a');
     _putchar('r');
     _putchar('\n');
+
     return (0);
 }
