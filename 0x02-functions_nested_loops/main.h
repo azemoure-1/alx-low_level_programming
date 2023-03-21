3f528fbd2abb30389a3441eca618c4e9070ad30a
azemoure-1 <a.azemour1@gmail.com>
2023-03-21 02:45:20 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+
+/* function prototypes */
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+
+#endif /* MAIN_H */
