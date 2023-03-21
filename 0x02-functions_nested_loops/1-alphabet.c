68c23433653221a232814db816c509e8fa91387d
azemoure-1 <a.azemour1@gmail.com>
2023-03-21 16:38:37 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,8 +10,8 @@ void print_alphabet(void)
     int ch;
 
     for (ch = 'a'; ch <= 'z'; ch++)
-	{
-		_putchar(ch);
-	}
-	_putchar('\n');
+    {
+        _putchar(ch);
+    }
+    _putchar('\n');
 }
