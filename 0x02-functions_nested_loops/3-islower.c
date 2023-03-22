99ba36297beab396dc949a121f5ee96c512c8c3d
azemoure-1 <a.azemour1@gmail.com>
2023-03-21 17:33:16 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+/**
+ * _islower - checks for lowercase character
+ * @c: the character to be checked
+ *
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
+
