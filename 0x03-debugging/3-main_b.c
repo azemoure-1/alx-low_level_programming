19223b374c6fd8a530c5145a0a021ffca1d68e12
azemoure-1 <a.azemour1@gmail.com>
2023-03-22 16:07:57 -0700

    tt

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -3,15 +3,6 @@ void print_remaining_days(int month, int day, int year)
     int i, total = 0;
     for (i = 1; i < month; i++)
     {
-        total += day_in_month(i, year);
-    }
-    total += day;
-
-    if ((is_leap_year(year) && month > 2) && (day >= 60 || (day == 59 && day_in_month(month, year) == 29)))
-    {
-        total++;
-    }
-
-    printf("Day of the year: %d\n", total);
+        totalotal);
 }
 
