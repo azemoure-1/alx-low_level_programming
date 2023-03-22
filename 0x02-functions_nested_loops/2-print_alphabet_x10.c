af9891d8e3cafd8c500f59fe6e2847aa5cf95de5
azemoure-1 <a.azemour1@gmail.com>
2023-03-21 17:27:06 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - a function that prints 10 times the alphabet, in lowercase,
+ * print_alphabet_x10 - a function that prints 10 times the alphabe,
  * followed by a new line.
  */
 
