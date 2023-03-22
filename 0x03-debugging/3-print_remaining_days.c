865c0c110895bf56f5ba0baa731c147235726c94
azemoure-1 <a.azemour1@gmail.com>
2023-03-22 05:43:01 -0700

    2-largest_number.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * print_remaining_days - Calculates the remaining days left in the year, taking leap years into account.
- * @month: month in number format.
- * @day: day of month.
- * @year: year.
+ * print_remaining_days - Calculates days left in the year, taking leap years into account
+ * @month: month in number format
+ * @day: day of month
+ * @year: year
  */
+
 void print_remaining_days(int month, int day, int year)
 {
 	/**
