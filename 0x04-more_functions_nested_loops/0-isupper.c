4ac43f20b782738bff22b10b7f847089489f0081
azemoure-1 <a.azemour1@gmail.com>
2023-03-23 03:52:39 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ * is upper - chek
+ * @c: input fo alphabet
+ * Return: 1 if uppercase. 0 if not
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _putchar(char c);
+int _isupper(int c);
+
+#endif
+
