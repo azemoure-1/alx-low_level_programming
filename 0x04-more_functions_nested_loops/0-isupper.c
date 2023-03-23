909fa0fd68b38bc311e96c99b6f5479fc814e6c7
azemoure-1 <a.azemour1@gmail.com>
2023-03-23 08:07:02 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,12 +6,17 @@
  * @c: input fo alphabet
  * Return: 1 if uppercase. 0 if not
  */
-int _isupper(int C)
+
+int _isupper(int c)
 {
     /* ASCII values for uppercase letters are between 65 and 90 */
 	if (c >= 65 && c <= 90)
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
 
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,7 +2,31 @@
 #define MAIN_H
 
 #include <stdio.h>
-int _isupper(int C);
+
+int _putchar(char C);
+
+void print_alphabet(void);
+
+void print_alphabet_x10(void);
+
+int _islower(int C);
+
+int _isalpha(int C);
+
+int print_sign(int n);
+
+int _abs(int);
+
+int print_last_digit(int);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int, int);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
 
 #endif
-
