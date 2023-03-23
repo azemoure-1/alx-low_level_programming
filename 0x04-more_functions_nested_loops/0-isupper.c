53b199b3a3253f3e418623dcef71848cdc391a84
azemoure-1 <a.azemour1@gmail.com>
2023-03-23 04:05:19 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,8 +8,10 @@
  */
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
-		return (1);
-	else
-		return (0);
+    /* ASCII values for uppercase letters are between 65 and 90 */
+    if (c >= 65 && c <= 90)
+        return 1;
+    else
+        return 0;
 }
+
