b90395bd1d81dcc1866568d551a996294db9a7e9
azemoure-1 <a.azemour1@gmail.com>
2023-03-27 10:12:17 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,41 +8,35 @@
  */
 int _atoi(char *s)
 {
-    int sign = 1;
-    int result = 0;
-    int i = 0;
+	int sign = 1;
+	int result = 0;
+	int i = 0;
 
-    while (s[i])
-    {
-        if (s[i] == '-')
-            sign *= -1;
-        else if (s[i] >= '0' && s[i] <= '9')
-        {
-            int digit = s[i] - '0';
-
-            if (result > INT_MAX / 10)
-            {
-                result = (sign == 1) ? INT_MAX : INT_MIN;
-                break;
-            }
-
-            result = result * 10 + digit;
-
-            if (digit == 0 && result == 0)
-                sign = 1;
-
-            if (result < 0)
-            {
-                result = (sign == 1) ? INT_MAX : INT_MIN;
-                break;
-            }
-        }
-        else if (result > 0)
-            break;
-
-        i++;
-    }
-
-    return (result * sign);
+	while (s[i])
+	{
+	if (s[i] == '-')
+		sign *= -1;
+	else if (s[i] >= '0' && s[i] <= '9')
+	{
+		int digit = s[i] - '0';
+		if (result > INT_MAX / 10)
+		{
+			result = (sign == 1) ? INT_MAX : INT_MIN;
+			break;
+		}
+		result = result * 10 + digit;
+		if (digit == 0 && result == 0)
+			sign
+				if (result < 0)
+				{
+					result = (sign == 1) ? INT_MAX : INT_MIN;
+					break;
+				}
+	}
+	else if (result > 0)
+		break;
+	i++;
+	}
+	return (result * sign);
 }
 
