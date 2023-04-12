1d17e1b05fae9e2e0e1276363fa3a7c331940e62
azemoure-1 <a.azemour1@gmail.com>
2023-04-11 17:06:57 -0700

    0x0B. C - malloc, free

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -3,43 +3,42 @@
 
 /**
  * argstostr - Concatenates all the arguments of the program
- * @ac: Number of arguments
- * @av: Pointer to array of size ac containing arguments
+ * @ac: The number of arguments
+ * @av: An array of pointers to the arguments
  *
- * Return: Pointer to the new concatenated string
+ * Return: If concatenation fails or ac == 0, return NULL.
+ *         Otherwise, return a pointer to the new string.
  */
 char *argstostr(int ac, char **av)
 {
-	int i, j, k = 0, len = 0;
 	char *str;
+	int i, j, k = 0, len = 0;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
 	for (i = 0; i < ac; i++)
 	{
-	for (j = 0; av[i][j]; j++)
-		len++;
+		for (j = 0; av[i][j]; j++)
+			len++;
 		len++;
 	}
 
-	str = malloc((len + 1) * sizeof(char));
+	str = malloc(sizeof(char) * (len + 1));
 
 	if (str == NULL)
 		return (NULL);
 
 	for (i = 0; i < ac; i++)
 	{
-	for (j = 0; av[i][j]; j++)
-	{
-		str[k] = av[i][j];
-		k++;
-	}
-	str[k] = '\n';
-	k++;
+		for (j = 0; av[i][j]; j++)
+			str[k++] = av[i][j];
+
+		str[k++] = '\n';
 	}
 
 	str[k] = '\0';
+
 	return (str);
 }
 
