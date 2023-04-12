205572cd526d4a1f39a4ad05223683f5813b4e95
azemoure-1 <a.azemour1@gmail.com>
2023-04-12 04:34:38 -0700

    test

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the program
+ * @argc: The number of arguments passed to the program
+ * @argv: An array of strings containing the arguments passed to the program
+ *
+ * Return: 0 if successful
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _putchar(char C);
+int main(int argc, char *argv[]);
+
+
+#endif
+
