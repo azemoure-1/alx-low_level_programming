244b87db2c4761b08de00e4d4165a902aa1cac2b
azemoure-1 <a.azemour1@gmail.com>
2023-04-13 06:32:04 -0700

    test

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * is_digit - checks if a string contains only digits
+ * @str: the string to check
+ *
+ * Return: 1 if str contains only digits, 0 otherwise
+ */
+int is_digit(char *str)
+{
+    int i = 0;
+
+    while (str[i] != '\0')
+    {
+        if (str[i] < '0' || str[i] > '9')
+            return (0);
+        i++;
+    }
+
+    return (1);
+}
+
+/**
+ * main - entry point
+ * @argc: the number of arguments passed to the program
+ * @argv: an array of strings containing the arguments passed to the program
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char **argv)
+{
+	int sum = 0, i;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+	if (!is_digit(argv[i]))
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
+
