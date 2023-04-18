d0436cc76f159fa14a5b3f76394b7ef40fcdf847
azemoure-1 <a.azemour1@gmail.com>
2023-04-17 18:16:56 -0700

    C - Structures, typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,13 +10,15 @@
  * Description: A structure representing a dog, with name, age, and owner
  *              information.
  */
-void free_dog(dog_t *d);
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* DOG_H */
 
