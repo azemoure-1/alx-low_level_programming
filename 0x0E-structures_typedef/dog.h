3d5964b2831dbbd92a841b917189cc07e13a3f74
azemoure-1 <a.azemour1@gmail.com>
2023-04-17 17:37:45 -0700

    test

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A structure representing a dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Description: A structure representing a dog, with name, age, and owner
+ *              information.
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+#endif /* DOG_H */
+
