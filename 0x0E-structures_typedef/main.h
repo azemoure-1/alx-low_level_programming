24bd494aeda21e24300946344719cc86c7a2954c
azemoure-1 <a.azemour1@gmail.com>
2023-04-17 18:04:44 -0700

    C - Structures, typedef

--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -6,7 +6,8 @@
 int _putchar(char C);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
 
