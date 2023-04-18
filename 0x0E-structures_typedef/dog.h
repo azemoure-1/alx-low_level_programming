ab3cff1ac3c21fbf22348677300ddc54490c1d84
azemoure-1 <a.azemour1@gmail.com>
2023-04-18 03:51:20 -0700

    a

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,4 @@
+#ifndef DOG_H
 #define DOG_H
 
 /**
@@ -16,7 +17,5 @@ struct dog
 	char *owner;
 };
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
 #endif
 
