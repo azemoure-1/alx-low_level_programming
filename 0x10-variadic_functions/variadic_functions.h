485813322044a4c89a0d377a96501c9fdd05d9f8
azemoure-1 <a.azemour1@gmail.com>
2023-04-20 06:04:23 -0700

    edit

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,6 +4,12 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+typedef struct token
+{
+	char *token 
+	void (*f)(char *, va_list);
+} token_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
